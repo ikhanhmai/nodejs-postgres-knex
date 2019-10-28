1561f2cd4d46badbae8cc942ba0fd84fff081994
khanh <ikhanhmai@gmail.com>
2019-10-28 11:38:26 +0700

    remove amz key

--- utilities/sendEmail.js
+++ utilities/sendEmail.js
@@ -1,7 +1,7 @@
 const aws = require('aws-sdk');
 // use AWS global variables
-aws.config.accessKeyId = 'AKIAI4LWPLHE6VWI5ANQ';
-aws.config.secretAccessKey = '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';
+aws.config.accessKeyId;
+aws.config.secretAccessKey;
 aws.config.region = 'us-east-1';
 
 // Create an Email function
